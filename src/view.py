10fbabb5391e7376d82c56c2b644c9db8f82a433
davdcsam <edavidcamposl@gmail.com>
2024-08-13 04:43:52 -0600

    ### Add URL print button to View class
    Added a button to the View class for printing the model URL, which enhances the current debugging process by facilitating easier inspection of the URL state. Simplified date formatting in date pickers and improved code consistency with naming conventions.

--- src/view.py
+++ src/view.py
@@ -1,39 +1,41 @@
 import ipywidgets as widgets
 from IPython.display import display
 
+
 class View:
     def __init__(self, view_model) -> None:
         self._view_model = view_model
 
-        self.start_DatePicker = widgets.DatePicker(
-            description='Start',
-            disabled=False
-        )
+        self.start_datepicker = widgets.DatePicker(description="Start", disabled=False)
+        self.end_datepicker = widgets.DatePicker(description="End", disabled=False)
+w        self.label_test = widgets.Label("Init")
+        self.print_url_button = widgets.Button(description="Print URL")
 
-        self.end_DatePicker = widgets.DatePicker(
-            description='End',
-            disabled=False
-        )
-
-        self.label_test = widgets.Label("Init")
-
-        self.start_DatePicker.observe(self._on_start_picker_change, names="value")
-        self.end_DatePicker.observe(self._on_end_picker_change, names="value")
+        self.start_datepicker.observe(self._on_start_picker_change, names="value")
+        self.end_datepicker.observe(self._on_end_picker_change, names="value")
+        self.print_url_button.on_click(self._print_url)
 
     def _on_start_picker_change(self, change):
         if change["new"] is not None:
-            self._view_model.start_date = str(change["new"])
+            self._view_model.start_date = change["new"].strftime("%b%d.%Y")
             self._update_label()
 
     def _on_end_picker_change(self, change):
         if change["new"] is not None:
-            self._view_model.end_date = str(change["new"])
+            self._view_model.end_date = change["new"].strftime("%b%d.%Y")
             self._update_label()
 
     def _update_label(self):
-        self.label_test.value = f"Start: {self._view_model.start_date}, End: {self._view_model.end_date}"
+        self.label_test.value = (
+            f"Start: {self._view_model.start_date}, End: {self._view_model.end_date}"
+        )
+
+    def _print_url(self, button):
+        # Imprime el valor actual de model.url
+        print(f"Current model URL: {self._view_model._model.url}")
 
     def display_datepickers(self):
-        display(self.start_DatePicker)
-        display(self.end_DatePicker)
+        display(self.start_datepicker)
+        display(self.end_datepicker)
         display(self.label_test)
+        display(self.print_url_button)
