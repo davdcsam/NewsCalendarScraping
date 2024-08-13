2137aa14871e292eae87be6a429721c1e2ce4fc5
davdcsam <edavidcamposl@gmail.com>
2024-08-13 02:58:32 -0600

    Add config constants for scraping and simplify utils
    Introduced a new configuration file for defining allowed element types, currency codes, and impact color mappings to enhance clarity and maintainability. This also streamlines the scraping process by clearly outlining the criteria for acceptable data.
    
    Additionally, cleaned up utility functions by removing unnecessary methods and refining parameter names for better readability and understanding. This change improves the overall efficiency of the code by keeping only essential functionality.
    
    No functionality is broken; instead, the overall organization will facilitate future enhancements and reduce the likelihood of errors.

--- src/utils.py
+++ src/utils.py
@@ -3,16 +3,6 @@ import re
 import json
 import pandas as pd
 
-def read_json(path):
-    """
-    Read JSON data from a file.
-    Args: path (str): The path to the JSON file.
-    Returns: dict: The loaded JSON data.
-    """
-    with open(path, 'r') as f:
-        data = json.load(f)
-    return data
-
 def contains_day_or_month(text):
     """
     Check if the given text contains a day of the week or a month.
@@ -38,50 +28,14 @@ def contains_day_or_month(text):
     matched_text = match.group(0)
     if re.match(days_of_week, matched_text, re.IGNORECASE):
         return True, matched_text
-
-def find_pattern_category(text):
-    """
-    Find the category of a specific pattern within the given text.
-
-    Args:
-        text (str): The input text to analyze.
-
-    Returns:
-        tuple: A tuple containing a boolean indicating whether a match was found,
-        the category of the matched pattern, and the matched text.
-    """
-
-    # Regular expressions for different patterns
-    time_pattern = r'\d{1,2}:\d{2}(am|pm)'
-    day_pattern = r'Day\s+\d+'
-    date_range_pattern = r'\d{1,2}(st|nd|rd|th)\s*-\s*\d{1,2}(st|nd|rd|th)'
-    tentative_pattern = r'\bTentative\b'
-    pattern = f'({time_pattern}|{day_pattern}|{date_range_pattern}|{tentative_pattern})'
-    match = re.search(pattern, text, re.IGNORECASE)
-
-    if not match:
-        return False,None,None
     
-    matched_text = match.group(0)
-    if re.match(time_pattern, matched_text, re.IGNORECASE):
-        category = "time"
-    elif re.match(day_pattern, matched_text, re.IGNORECASE):
-        category = "day_reference"
-    elif re.match(date_range_pattern, matched_text, re.IGNORECASE):
-        category = "date_range"
-    elif re.match(tentative_pattern, matched_text, re.IGNORECASE):
-        category = "tentative"
-    else:
-        category = "Unknown"
-    return True, category, matched_text
-    
-def reformat_scraped_data(data,month):
+def reformat_scraped_data(data: list ,filename: str ) -> pd.DataFrame:
     """
     Reformat scraped data and save it as a DataFrame and a CSV file.
 
     Args:
         data (list): The scraped data as a list of lists.
-        month (str): The month for naming the output CSV file.
+        filename (str): Filename the output CSV file.
 
     Returns:
         pd.DataFrame: The reformatted data as a DataFrame.
@@ -109,6 +63,6 @@ def reformat_scraped_data(data,month):
 
     df = pd.DataFrame(structured_rows,columns=['date','time','currency','impact','event'])
     os.makedirs("news",exist_ok=True)
-    df.to_csv(f"news/{month}_news.csv",index=False)
+    df.to_csv(f"news/{filename}.csv",index=False)
 
     return df
